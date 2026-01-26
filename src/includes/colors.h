c6c144fa5b306bed34f2a7bff24aa7fb7ba2d882
gnom2949 <sasasilaev27@gmail.com>
2026-01-26 12:39:26 +0500

    Colors function, uses macroses

---
+++ src/includes/colors.h
@@ -0,0 +1,35 @@
+/* colors.h
+ *
+ * Copyright 2026 Int Software, Aleksandr Silaev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * IUTF Colors version v0.1
+ */
+
+#ifndef IUTF_COLORS_H
+#define IUTF_COLORS_H
+
+// Colors mark \\ START
+#define COL_RED "\033[31m"
+#define COL_GRN "\033[32m"
+#define COL_YLW "\033[33m"
+#define COL_CYAN "\033[36m"
+#define COL_BLU "\033[34m"
+#define COL_PRPL "\033[35m"
+#define COL_WHITE "\033[37m"
+#define COL_BLACK "\033[30m"
+#define COL_DEF "\033[0m"
+// Colors mark \\ END
+#endif
